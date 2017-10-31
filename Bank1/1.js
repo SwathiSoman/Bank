4eb5e97eca01cceabada39e366eed349ec9738da
SwathiSoman <30813068+SwathiSoman@users.noreply.github.com>
2017-11-01 11:10:05 +1300

    Add files via upload

---
+++ Bank1/1.js
@@ -0,0 +1,117 @@
+
+var myvar = 'package tdd.by.example;'+''+'
+import org.junit.Test;'+''+'
+import static org.junit.Assert.assertEquals;'+'
+import static org.junit.Assert.assertFalse;'+'
+import static org.junit.Assert.assertTrue;';
+
+var myvar = 'public class MoneyExampleTest {'+''+'    @Test'+'    
+public void testMultiplication() {'+'        
+Money five = Money.dollar(5);'+'       
+assertEquals(Money.dollar(10), five.times(2));'+'        
+assertEquals(Money.dollar(15), five.times(3));'+'    
+}';
+
+
+var myvar = '@Test'+' public void testEquality() {'+'        
+assertTrue(Money.dollar(5).equals(Money.dollar(5)));'+'        
+assertFalse(Money.dollar(5).equals(Money.dollar(6)));'+'        
+assertFalse(Money.franc(5).equals(Money.dollar(5)));'+' 
+   }';
+
+var myvar = '@Test'+'    
+public void testCurrency() {'+'       
+assertEquals("USD", Money.dollar(1).currency());'+'       
+assertEquals("CHF", Money.franc(1).currency());'+'   
+ }
+ 
+ var myvar = '@Test'+' 
+ public void testSimpleAddition() {'+'      
+ Money five = Money.dollar(5);'+'        
+ Expression sum = five.plus(five);'+'        
+ Bank bank = new Bank();'+'      
+ Money reduced = bank.reduce(sum, "USD");'+'        
+ assertEquals(Money.dollar(10), reduced);'+' 
+ }';
+ 
+ var myvar = '@Test'+'    
+ public void testPlusReturnsSum() {'+'        
+ Money five = Money.dollar(5);'+'        
+ Expression result = five.plus(five);'+'      
+ Sum sum = (Sum)result;'+'        
+ assertEquals(five, sum.augend);'+'        
+ assertEquals(five, sum.addend);'+'  
+ }';
+	
+
+var myvar = '@Test'+'    
+public void testReduceSum() {'+'        
+Expression sum = new Sum(Money.dollar(3), Money.dollar(4));'+'      
+Bank bank = new Bank();'+'       
+Money result = bank.reduce(sum, "USD");'+'        
+assertEquals(Money.dollar(7), result);'+'  
+  }';
+  
+ 
+var myvar = '@Test'+'    
+public void testReduceMoney() {'+'        
+Bank bank = new Bank();'+'       
+Money result = bank.reduce(Money.dollar(1), "USD");'+'        
+assertEquals(Money.dollar(1), result);'+'    
+}';
+
+var myvar = '@Test'+'    
+public void testReduceMoneyDifferentCurrency() {'+'        
+Bank bank = new Bank();'+'        
+bank.addRate("CHF", "USD", 2);'+'        
+Money result = bank.reduce(Money.franc(2), "USD");'+'       
+assertEquals(Money.dollar(1), result);'+'  
+  }';
+
+  
+var myvar = '@Test'+'   
+public void testIdentityRate() {'+'        
+assertEquals(1, new Bank().rate("USD", "USD"));'+'   
+ }';
+ 
+ 
+
+var myvar = '@Test'+'   
+public void testMixedAddition() {'+'        
+Expression fiveBucks = Money.dollar(5);'+'       
+Expression tenFrancs = Money.franc(10);'+'   
+Bank bank = new Bank();'+'      
+bank.addRate("CHF", "USD", 2);'+'       
+Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");'+'        
+assertEquals(Money.dollar(10), result);'+'   
+ }';
+ 
+ var myvar = '@Test'+'   
+ public void testSumPlusMoney() {'+'      
+ Expression fiveBucks = Money.dollar(5);'+'      
+ Expression tenFrancs = Money.franc(10);'+'       
+ Bank bank = new Bank();'+'      
+ bank.addRate("CHF", "USD", 2);'+'      
+ Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);'+'       
+ Money result = bank.reduce(sum, "USD");'+'       
+ assertEquals(Money.dollar(15), result);'+'    
+ }';
+ 
+ var myvar = '@Test'+'    
+ public void testSumTimes() {'+'        
+ Expression fiveBucks = Money.dollar(5);'+'       
+ Expression tenFrancs = Money.franc(10);'+'       
+ Bank bank = new Bank();'+'      
+ bank.addRate("CHF", "USD", 2);'+'       
+ Expression sum = new Sum(fiveBucks, tenFrancs).times(2);'+'       
+ Money result = bank.reduce(sum, "USD");'+'       
+ assertEquals(Money.dollar(20), result);'+'  
+ }'+''+'}';
+	
+
+	
+
+
+	
+
+	
